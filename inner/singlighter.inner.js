50fa28d437cb684b1005a69f6403f656f7f48608
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-08-24 13:50:04 +0330

    serve command added

--- inner/singlighter.inner.js
+++ inner/singlighter.inner.js
@@ -1,5 +1,5 @@
 const fs  = require("fs");
-// const https = require("https");
+const http = require("http");
 
 const command = process.argv[2];
 const archive = {
@@ -23,8 +23,13 @@ switch (command) {
             makePageCmd(process.argv[3]);
         break;
     }
+    case "serve": {
+        serveCmd();
+        break;
+    }
     default: {
-        errorHelper(`Bad usage:\n\t'${command}' is not a command`)
+        errorHelper(`Bad usage:\n\t'${command}' is not a command`);
+        break;
     }
 }
 
@@ -39,4 +44,25 @@ function successHelper(text) {
 function makePageCmd(pageName) {
     fs.writeFileSync(`./Scripts/Pages/${pageName}.js`, archive.page.replace("{{PAGENAME}}", pageName));
     successHelper("Page was created successfully")
+}
+
+function serveCmd() {
+    successHelper("Server was started on [http://localhost:8000] - Press Ctrl+C for stop server")
+
+    const serverHandler = (req, res) => {
+        req.url = "." + req.url;
+        if (!fs.existsSync(req.url)) req.url = "/";
+        url = fs.lstatSync(req.url).isFile() ? "./" + req.url : `./${req.url}/index.html`;
+        let output = fs.readFileSync(url);
+        res.write(output);
+        res.end();
+    };
+
+    let server = http.createServer(serverHandler).listen(8000);
+
+    fs.watch(".", () => {
+        successHelper("Files was modified, Server was restarted");
+        server.close();
+        server = http.createServer(serverHandler).listen(8000);
+    })
 }
